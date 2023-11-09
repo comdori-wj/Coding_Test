2a88b887276e024280c62a6635e5e9ee9e3c5a68
COMDORI <02145s1@gmail.com>
2023-11-09 14:11:25 +0900

    문자열 출력하기

---
+++ Swift/문자열 출력하기/문자열 출력하기/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  문자열 출력하기
+//
+//  Created by Wonji Ha on 2023/11/09.
+//
+
+/**
+ * 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+ * 제한사항
+ 1 ≤ str의 길이 ≤ 1,000,000
+ str에는 공백이 없으며, 첫째 줄에 한 줄로만 주어집니다.
+ */
+
+import Foundation
+
+let str: String = readLine() ?? ""
+print(str)
